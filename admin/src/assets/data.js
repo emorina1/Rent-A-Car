3d0fef94f7163dbe9396c76c7f13a65f043b5c00
emorina1 <emorina1@example.com>
2024-12-12 00:10:47 +0100

    Create Admin Panel

---
+++ admin/src/assets/data.js
@@ -0,0 +1,48 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+import p5_img from './product5.png'
+import p6_img from './product6.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Aesop Care Product",
+    image:p1_img,
+    new_price:50.00,
+    old_price:80.50,
+  },
+  {id:2,
+    name:"Aesop Care Product",
+    image:p2_img,
+    new_price:85.00,
+    old_price:120.50,
+  },
+  {id:3,
+    name:"Aesop Care Product",
+    image:p3_img,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"Aesop Care Product",
+    image:p4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+  {id:5,
+    name:"Aesop Care Product",
+    image:p5_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+  {id:6,
+    name:"Aesop Care Product",
+    image:p6_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
---
+++ admin/src/assets/new_collections.js
@@ -0,0 +1,54 @@
+import p1_img from "./C2.png";
+import p2_img from "./c.png";
+import p3_img from "./c1.png";
+import p4_img from "./c3.png";
+import p5_img from './C2.png'
+import p6_img from './c.png'
+import p7_img from './c1.png'
+import p8_img from './c3.png'
+
+
+
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Rent a Car",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "Rent a Car",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Rent a Car",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 8,
+    name: "Rent a Car",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 1,
+    name: "Rent a Car",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+
+
+
+];
+
+export default new_collections;
